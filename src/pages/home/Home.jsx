fad018a4176a3e20744662a34e7c3ac5a8f426e6
amitroy4 <amitroyrock6071@gmail.com>
2023-09-24 00:01:17 +0600

    Experiance and education done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,9 +10,11 @@ import './home.css'
 import { FiEdit } from 'react-icons/fi';
 import { FaLocationArrow } from 'react-icons/fa';
 import { BiLogoLinkedinSquare } from 'react-icons/bi';
+import { useSelector } from 'react-redux';
 
 
 const Home = () => {
+    let userData = useSelector((state) => state.loginUser.loginUser)
     return (
         <div className="container">
             <div className="profile">
@@ -25,11 +27,11 @@ const Home = () => {
                 </div>
                 <div className="profileinfo">
                     <div className="propic">
-                        <img src="/propic.jpeg" alt="" />
+                        <img src={userData.photoURL} alt="" />
                     </div>
                     <div className="info">
                         <div className="nameloc">
-                            <h2>Dmitry Kargaev <BiLogoLinkedinSquare className='nameicon' /></h2>
+                            <h2>{userData.displayName}<BiLogoLinkedinSquare className='nameicon' /></h2>
                             <div className="loc"> <FaLocationArrow className='locicon' /> <span className='locedit'>Saint Petersburg, Russian Federation</span></div>
                         </div>
                         <div className="details">
@@ -61,7 +63,68 @@ const Home = () => {
                     <img src="/ext.png" alt="" />
                     <img src="/ext.png" alt="" />
                 </div>
+            </div>
+            <div className="experiance">
+                <h4>Experiance</h4>
+                <div className="expfield">
+                    <div className="explogo">
+                        <img src="/ext.png" alt="" />
+                    </div>
+                    <div className="expinfo">
+                        <div className="exptitle">
+                            Freelance UX/UI designer
+                        </div>
+                        <div className="expbox">
+                            <div className="left">Self Employed</div>
+                            <div className="right">Around the world</div>
+                        </div>
+                        <div className="expbox">
+                            <div className="left">Jun 2016 — Present</div>
+                            <div className="right rightcolor">3 yrs 3 mos</div>
+                        </div>
+                        <div className="expdetails">
+                            Work with clients and web studios as freelancer.  Work in next areas: eCommerce web projects; creative landing pages; iOs and Android apps; corporate web sites and corporate identity sometimes.
+                        </div>
+                    </div>
+                </div>
+                <div className="expfield">
+                    <div className="explogo">
+                        <img src="/ext.png" alt="" />
+                    </div>
+                    <div className="expinfo">
+                        <div className="exptitle">
+                            UX/UI designer
+                        </div>
+                        <div className="expbox">
+                            <div className="left">Upwork</div>
+                            <div className="right">International</div>
+                        </div>
+                        <div className="expbox">
+                            <div className="left">Jun 2019 — Present</div>
+                            <div className="right rightcolor">3 mos</div>
+                        </div>
+                        <div className="expdetails">
+                            New experience with Upwork system. Work in next areas: UX/UI design, graphic design, interaction design, UX research.
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div className="education">
+                <div className="h4">Education</div>
+                <div className="edufield">
+                    <div className="edulogo">
+                        <img src="/ext.png" alt="" />
+                    </div>
+                    <div className="eduinfo">
+                        <div className="title">Moscow State Linguistic University</div>
+                        <div className="degree">
+                            Bachelor's degree Field Of StudyComputer and Information Systems Security/Information Assurance
+                        </div>
+                        <div className="year">2013 — 2017</div>
+                        <div className="courses">Additional English classes and UX profile courses​.</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
